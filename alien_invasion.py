dc75e61e0122291485d8a7c48c903181c8119896
Novadin Abdi <nabdi30@student.cscc.edu>
2026-08-12 19:41:50 -0400

    fix play button

--- alien_invasion.py
+++ alien_invasion.py
@@ -81,23 +81,26 @@ class AlienInvasion:
                 self._check_play_button(mouse_pos)
 
     def _check_play_button(self, mouse_pos):
-        """Start a new game when the player clicks Play."""
+        """Start a new game when the player clicks Play.(me)"""
         button_clicked = self.play_button.rect.collidepoint(mouse_pos)
         if button_clicked and not self.game_active:
             # Reset the game statistics.
             self.stats.reset_stats()
+            self.sb.prep_score()
+            self.sb.prep_high_score()
+            self.sb.prep_ships()
             self.game_active = True
 
             # Get rid of any remaining bullets and aliens.
             self.bullets.empty()
             self.aliens.empty()
 
-            # Create a new fleet and center the ship.
+            # Create a new fleet and reset the ship.
             self._create_fleet()
             self.ship.center_ship()
 
-            # Hide the mouse cursor.
-            pygame.mouse.set_visible(False)
+        # Hide the mouse cursor.
+        pygame.mouse.set_visible(False)
 
     def _check_keydown_events(self, event):
         """Respond to keypresses.(me)"""
